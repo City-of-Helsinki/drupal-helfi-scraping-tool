b26719354c7c54153a20e2200da015eea4d8ce8c
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2024-06-10 08:57:56 +0300

    Improve example crawler

--- app/crawls/_example.py
+++ app/crawls/_example.py
@@ -10,7 +10,6 @@ regex_path_exclude_pattern = None # This turns off exclude filtering
 regex_content_include_pattern = None # This turns off include filtering
 regex_content_exclude_pattern = None # This turns off exclude filtering
 
-
 # What files to include based on path
 # regex_path_include_pattern = r'/kasvatus-ja-koulutus/' # Kasko
 # regex_path_include_pattern = r'/kaupunkiymparisto-ja-liikenne/' # Liikenne
@@ -20,31 +19,25 @@ exclude_paging = '(\d[a-f\d][a-f\d][a-f\d]|[a-f\d]\d[a-f\d][a-f\d]|[a-f\d][a-f\d
 exclude_news = '(\/uutiset\/|\/nyheter\/|\/news\/)'
 exclude_error = '(illustration_error_page_403_401|illustration_error_page_404)'
 
-
 # What files to exluce (After inclusion) based on path
-# regex_path_exclude_pattern =  r''+exclude_paging+''
+regex_path_exclude_pattern =  r''+exclude_paging+''
 # regex_path_exclude_pattern =  r'('+exclude_news+'|'+exclude_paging+')'
 
 # What files to include based on file content
-# regex_content_include_pattern = r'content-card--design-teaser'
-# regex_content_include_pattern = r'form' # Filter only pages that have at least one form
+# regex_content_include_pattern = r'body'
 
 # What files to exclude based on file content
-# regex_content_exclude_pattern = r'(openid-connect-login-form|user-login-form|eu-cookie-compliance-block-form)' # Filter out login forms and cookie consent forms
 # regex_content_exclude_pattern = r'hero__container'
 
 # Custom beautiful soup loop function
 def custom_soup_and_loop_logic(spider, response_body, url, BeautifulSoup):
-
-
-    soup2 = BeautifulSoup(response_body, 'html.parser') # Create a BeautifulSoup object from the HTML content
-    metas = soup2.select('meta[name="description"][content]')
-    metaContent = '';
-    for meta in metas:
-        metaContent = meta['content']
-
-
     soup = BeautifulSoup(response_body, 'html.parser') # Create a BeautifulSoup object from the HTML content
+
+    # Example of how to get meta data from page head
+    # metas = soup.select('meta[name="description"][content]')
+    # metaContent = '';
+    # for meta in metas:
+    #     metaContent = meta['content']
 
     # CSS selector for matching elements
     css_selector = 'body'
@@ -57,7 +50,6 @@ def custom_soup_and_loop_logic(spider, response_body, url, BeautifulSoup):
         yield {
             'url': url,
             # 'class': class_attr if class_attr is not None else '',
-            # 'title': match['title'],
             # 'title': match['title'],
             # "html": str(match), # Get outer   HTML
             # "inner_html": match.encode_contents().decode('utf-8').strip(),  # Get inner HTML
